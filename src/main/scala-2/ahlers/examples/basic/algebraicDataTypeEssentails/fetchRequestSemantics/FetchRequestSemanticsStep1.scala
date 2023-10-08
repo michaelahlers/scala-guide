b20d4d2363ee249521167fb9ec325023a69c6f76
Michael Ahlers <michael@ahlers.consulting>
2023-10-08 13:41:30 -0600

    WIP sketch

--- src/main/scala-2/ahlers/examples/basic/algebraicDataTypeEssentails/fetchRequestSemantics/FetchRequestSemanticsStep1.scala
+++ src/main/scala-2/ahlers/examples/basic/algebraicDataTypeEssentails/fetchRequestSemantics/FetchRequestSemanticsStep1.scala
@@ -1,3 +1,9 @@
 package ahlers.examples.basic.algebraicDataTypeEssentails.fetchRequestSemantics
 
-object FetchRequestSemanticsStep1 {}
+object FetchRequestSemanticsStep1 {
+
+  case class RequestUsers(
+    givenName: Option[String],
+    familyName: Option[String])
+
+}
