65c95dd80a18a599aba8cc7719dbabfc3bbe4737
Michael Ahlers <michael@ahlers.consulting>
2023-10-14 18:54:42 -0600

    Simplify

--- src/main/scala/advice/useDiffx/Product.scala
+++ src/main/scala/advice/useDiffx/Product.scala
@@ -1,6 +1,9 @@
 package advice.useDiffx
 
-import advice.useDiffx.Product.{Kind, Make, Model, Year}
+import advice.useDiffx.Product.Kind
+import advice.useDiffx.Product.Make
+import advice.useDiffx.Product.Model
+import advice.useDiffx.Product.Year
 
 case class Product(
   kind: Kind,
@@ -14,6 +17,6 @@ object Product {
   case class Kind(toText: String) extends AnyVal
   case class Make(toText: String) extends AnyVal
   case class Model(toText: String) extends AnyVal
-  case class Year(toInt: String) extends AnyVal
+  case class Year(toInt: Int) extends AnyVal
 
 }
