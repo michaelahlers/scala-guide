188f9213b2c6e67c42ce21721a966c28c0a21001
Michael Ahlers <michael@ahlers.consulting>
2023-10-22 17:23:28 -0600

    Simplify

--- src/main/scala/tutorial/monads/setup/Reservation.scala
+++ src/main/scala/tutorial/monads/setup/Reservation.scala
@@ -1,14 +1,12 @@
 package tutorial.monads.setup
 
 /**
- * @param locator Required identifier for this reservation.
  * @param passenger Required person information.
  * @param flight Required flight information.
  * @param ticket Optional ticketing information.
  * @param seat Optional seat assignment.
  */
 case class Reservation(
-  locator: String,
   passenger: Passenger,
   flight: Flight,
   ticket: Ticket,
