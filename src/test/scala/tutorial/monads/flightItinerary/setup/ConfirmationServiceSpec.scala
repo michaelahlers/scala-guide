894626d8df5fb3ba377fe0f528a9cd9f6a3d922b
Michael Ahlers <michael@ahlers.consulting>
2023-10-29 14:31:27 -0600

    Set up tests

--- src/test/scala/tutorial/monads/flightItinerary/setup/ConfirmationServiceSpec.scala
+++ src/test/scala/tutorial/monads/flightItinerary/setup/ConfirmationServiceSpec.scala
@@ -1,8 +1,41 @@
 package tutorial.monads.flightItinerary.setup
 
-class ConfirmationServiceSpec {}
+import org.scalamock.scalatest.MockFactory
+import org.scalatest.funspec.FixtureAnyFunSpec
+
+class ConfirmationServiceSpec extends FixtureAnyFunSpec with MockFactory {
+  import ConfirmationServiceSpec.Fixtures
+
+  override protected type FixtureParam = Fixtures
+
+  override protected def withFixture(test: OneArgTest) = {
+    val reservationService = mock[ReservationService]
+    val ticketService = mock[TicketService]
+    val seatService = mock[SeatService]
+
+    val confirmationService = ConfirmationService(
+      reservationService = reservationService,
+      ticketService = ticketService,
+      seatService = seatService,
+    )
+
+    test(Fixtures(
+      reservationService = reservationService,
+      ticketService = ticketService,
+      seatService = seatService,
+      confirmationService = confirmationService,
+    ))
+  }
+}
 
 object ConfirmationServiceSpec {
+
+  case class Fixtures(
+    reservationService: ReservationService,
+    ticketService: TicketService,
+    seatService: SeatService,
+    confirmationService: ConfirmationService,
+  )
 
   object samples {
 
