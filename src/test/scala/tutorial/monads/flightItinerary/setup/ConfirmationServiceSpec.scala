3a9fc647cb3c5e79262a7738d74fe28eef2e0e3d
Michael Ahlers <michael@ahlers.consulting>
2023-10-29 16:21:59 -0600

    Finish tests

--- src/test/scala/tutorial/monads/flightItinerary/setup/ConfirmationServiceSpec.scala
+++ src/test/scala/tutorial/monads/flightItinerary/setup/ConfirmationServiceSpec.scala
@@ -1,22 +1,82 @@
 package tutorial.monads.flightItinerary.setup
 
+import com.softwaremill.diffx.scalatest.DiffShouldMatcher.convertToAnyShouldMatcher
 import org.scalamock.scalatest.MockFactory
 import org.scalatest.flatspec.FixtureAnyFlatSpec
+import org.scalatest.matchers.should.Matchers._
 
 class ConfirmationServiceSpec extends FixtureAnyFlatSpec with MockFactory {
   import ConfirmationServiceSpec.Fixtures
+  import ConfirmationServiceSpec.samples
+
+  "No reservation" should "return null" in { fixtures =>
+    import fixtures._
+
+    var description: String = null
+
+    confirmationService
+      .getConfirmation(samples.noReservation.locator) { confirmation =>
+        description = ConfirmationService.describe(confirmation)
+      }
+
+    description.shouldBe(null)
+  }
+
+  "Not ticketed" should "return null" in { fixtures =>
+    import fixtures._
+
+    var description: String = null
+
+    confirmationService.getConfirmation(samples.notTicketed.locator) { confirmation =>
+      description = ConfirmationService.describe(confirmation)
+    }
+
+    description.shouldBe(null)
+  }
+
+  "Ticketed, not seated" should "return description" in { fixtures =>
+    import fixtures._
+
+    var description: String = null
+
+    confirmationService.getConfirmation(samples.ticketedNotSeated.locator) { confirmation =>
+      description = ConfirmationService.describe(confirmation)
+    }
+
+    description.shouldMatchTo("Passenger Grace Hopper is confirmed on flight from PIT to DEN has not been assigned a seat.")
+  }
+
+  "Ticketed, seated" should "return description" in { fixtures =>
+    import fixtures._
+
+    var description: String = null
+
+    confirmationService.getConfirmation(samples.ticketedSeated.locator) { confirmation =>
+      description = ConfirmationService.describe(confirmation)
+    }
+
+    description.shouldMatchTo("Passenger Grace Hopper is confirmed on flight from PIT to DEN has been assigned 14 C.")
+  }
 
   override protected type FixtureParam = Fixtures
   override protected def withFixture(test: OneArgTest) = {
-    import samples.noReservation
+    import samples.notTicketed
+    import samples.ticketedNotSeated
+    import samples.ticketedSeated
 
     val reservationService = ReservationService(Map(
+      notTicketed.locator -> notTicketed.reservation,
+      ticketedNotSeated.locator -> ticketedNotSeated.reservation,
+      ticketedSeated.locator -> ticketedSeated.reservation,
     ))
 
     val ticketService = TicketService(Map(
+      ticketedNotSeated.locator -> ticketedNotSeated.ticket,
+      ticketedSeated.locator -> ticketedSeated.ticket,
     ))
 
     val seatService = SeatService(Map(
+      ticketedSeated.locator -> ticketedSeated.seat,
     ))
 
     val confirmationService = ConfirmationService(
@@ -26,9 +86,6 @@ class ConfirmationServiceSpec extends FixtureAnyFlatSpec with MockFactory {
     )
 
     test(Fixtures(
-      reservationService = reservationService,
-      ticketService = ticketService,
-      seatService = seatService,
       confirmationService = confirmationService,
     ))
   }
@@ -37,9 +94,6 @@ class ConfirmationServiceSpec extends FixtureAnyFlatSpec with MockFactory {
 object ConfirmationServiceSpec {
 
   case class Fixtures(
-    reservationService: ReservationService,
-    ticketService: TicketService,
-    seatService: SeatService,
     confirmationService: ConfirmationService,
   )
 
