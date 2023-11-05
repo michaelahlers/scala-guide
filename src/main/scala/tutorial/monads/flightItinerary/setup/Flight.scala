52b948a92ec8152b07121d933150f3d6a53303d4
Michael Ahlers <michael@ahlers.consulting>
2023-11-05 14:09:08 -0700

    More comments

--- src/main/scala/tutorial/monads/flightItinerary/setup/Flight.scala
+++ src/main/scala/tutorial/monads/flightItinerary/setup/Flight.scala
@@ -1,5 +1,14 @@
 package tutorial.monads.flightItinerary.setup
 
+/**
+ * Describe what flight is performing service for a particular market as part of a [[Reservation]].
+ *
+ * @param number Identifies a particular flight.
+ * @param origin Departure airport code (''e.g'', `DEN`, `PIT`, `SFO`).
+ * @param destination Arrival airport code.
+ *
+ * @see [[https://en.wikipedia.org/wiki/IATA_airport_code IATA airport code]]
+ */
 case class Flight(
   number: Int,
   origin: String,
