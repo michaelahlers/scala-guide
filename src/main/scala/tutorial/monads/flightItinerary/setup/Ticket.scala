47478072dafbfe03f8b7a3ef0fc841f7098ef656
Michael Ahlers <michael@ahlers.consulting>
2023-11-05 13:30:43 -0700

    More comments

--- src/main/scala/tutorial/monads/flightItinerary/setup/Ticket.scala
+++ src/main/scala/tutorial/monads/flightItinerary/setup/Ticket.scala
@@ -1,5 +1,12 @@
 package tutorial.monads.flightItinerary.setup
 
+/**
+ * A confirmed ticket for a [[Reservation]]. A possibly little-known fact is these are not always assigned at the time a flight is booked.
+ *
+ * ''That'' must be taken into consideration for our purposes here: a [[Reservation]] may exist without a confirmed [[Ticket]].
+ *
+ * @see [[https://lifehacker.com/make-sure-your-flight-reservation-is-ticketed-before-yo-1836791737 Make Sure Your Flight Reservation Is Ticketed Before You Travel]]
+ */
 case class Ticket(
   number: Int,
 )
