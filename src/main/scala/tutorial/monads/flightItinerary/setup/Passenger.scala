ee1b874179b3df3716cdd7e5c18bc96e8408bafb
Michael Ahlers <michael@ahlers.consulting>
2023-11-05 14:07:08 -0700

    Comment

--- src/main/scala/tutorial/monads/flightItinerary/setup/Passenger.scala
+++ src/main/scala/tutorial/monads/flightItinerary/setup/Passenger.scala
@@ -1,5 +1,10 @@
 package tutorial.monads.flightItinerary.setup
 
+/**
+ * Describe who's traveling as part of a [[Reservation]].
+ *
+ * @param name A person's name.
+ */
 case class Passenger(
   name: String,
 )
